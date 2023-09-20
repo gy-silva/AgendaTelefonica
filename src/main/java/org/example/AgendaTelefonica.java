c6ad548f3d2c4b62b3bbc7d78654de14277941a7
Gisele Silva <g.sabrina45@gmail.com>
2023-09-20 15:42:19 -0300

    desafio agenda telefonica

---
+++ src/main/java/org/example/AgendaTelefonica.java
@@ -0,0 +1,64 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AgendaTelefonica {
+    private List<Contato> contatos;
+
+    public AgendaTelefonica() {
+
+        contatos = new ArrayList<>();
+    }
+
+    public void adicionarContato(Contato contato) {
+
+        contatos.add(contato);
+    }
+
+    public void removerContato(String nome) {
+        Contato contatoParaRemover = null;
+        for (Contato contato : contatos) {
+            if (contato.getNome().equalsIgnoreCase(nome)) {
+                contatoParaRemover = contato;
+                break;
+            }
+        }
+        if (contatoParaRemover != null) {
+            contatos.remove(contatoParaRemover);
+        } else {
+            System.out.println("Contato não encontrado.");
+        }
+    }
+
+    public Contato buscarContato(String nome) {
+        for (Contato contato : contatos) {
+            if (contato.getNome().equalsIgnoreCase(nome)) {
+                return contato;
+            }
+        }
+        return null;
+    }
+
+    public void atualizarContato(String nome, Contato novoContato) {
+        for (int i = 0; i < contatos.size(); i++) {
+            Contato contato = contatos.get(i);
+            if (contato.getNome().equalsIgnoreCase(nome)) {
+                contatos.set(i, novoContato);
+                return;
+            }
+        }
+        System.out.println("Contato não encontrado.");
+    }
+
+    public void listarContatos() {
+        if (contatos.isEmpty()) {
+            System.out.println("A agenda está vazia.");
+        } else {
+            System.out.println("Contatos na agenda:");
+            for (Contato contato : contatos) {
+                System.out.println(contato);
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        MenuUsuario menuUsuario = new MenuUsuario();
+        menuUsuario.menu();
+    }
+}
